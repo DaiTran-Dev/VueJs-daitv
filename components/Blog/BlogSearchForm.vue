753c981a2fd47b54b77c01f0f8acaeede56d03c1
DaiTran-Dev <daivitran.dev@gmail.com>
2021-04-20 19:00:19 +0700

    update ok

--- components/Blog/BlogSearchForm.vue
+++ components/Blog/BlogSearchForm.vue
@@ -6,19 +6,26 @@
         <div class="form-group">
           <label for="inputPassword6">Tiêu Đề</label>
           <input
-            type="password"
+            type="text"
             id="inputPassword6"
             class="form-control mx-sm-3"
             placeholder="Tiêu Đề"
+            v-model="searchText"
           />
         </div>
         <div class="form-group">
-          <button type="button" class="btn btn-primary">Search</button>
+          <button type="button" class="btn btn-primary" @click="$emit('btn-click',searchText)">Search</button>
         </div>
       </form>
     </div>
   </div>
 </template>
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      searchText: "",
+    };
+  },
+};
 </script>
--- pages/blogList/search.vue
+++ pages/blogList/search.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <blogSearchForm></blogSearchForm>
-    <blogListTable></blogListTable>
+    <blogSearchForm @btn-click="searchText = $event"></blogSearchForm>
+    <blogListTable :posts="posts"></blogListTable>
   </div>
 </template>
 <script>
@@ -10,11 +10,43 @@ import blogListTable from "../../components/Blog/BlogListTable.vue";
 
 export default {
   data() {
-    return {};
+    return {
+      searchText:'',
+      posts:[],
+    };
+  },
+  methods: {
+    async searchAll() {
+      const response = await this.$axios.$get("http://localhost:3000/blogs");
+      this.posts = response;
+    },
+    async searchWhere(strValue) {
+      const response = await this.$axios.$get("http://localhost:3000/blogs");
+      var listData = new Array(response)[0];
+      this.posts = listData.filter(function(value){
+        var str = value.title;
+        if(str.indexOf(strValue)!=-1){
+          return true;
+        }
+        return false;
+      });
+    },
   },
   components: {
     blogSearchForm,
     blogListTable,
   },
+  watch:{
+    searchText:function(val){
+      if(val.trim()!=""){
+        this.searchWhere(val.trim())
+      }else{
+        this.searchAll();
+      }
+    }
+  },
+  mounted(){
+    this.searchAll();
+  }
 };
 </script>
